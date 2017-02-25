e88c0ca9dc1c7d7e3cd758cdf0846ecb34e40ca7
Anusha917 <anusha39393@gmail.com>
2017-02-25 14:21:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,16 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 
 element.innerHTML="New Value";
+
+//to move image
+var img=document.getElementById("madi");
+var marginLeft=0;
+//function moveRight(){
+   moveLeft=moveLeft+10;
+    img.style.moveLeft=moveLeft+'px';
+}
+
+img.onClick=function(){
+    //var interval=setInterval(moveRight,100);
+    img.style.moveLeft="100px";
+}
