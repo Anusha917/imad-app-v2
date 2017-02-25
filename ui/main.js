e312efbe1ca6f71e0fcac2a9a3206ea63f883e86
Anusha917 <anusha39393@gmail.com>
2017-02-25 21:28:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,9 +17,12 @@ img.onclick=function(){
     //img.style.marginLeft='100px';//to move image once
 };*///module p5 code
 var button=document.getElementById("counter");
+var counter=0;
 button.onclick =function(){
     //make a request to counter endpoint
+    
     //capture the response and store it in a variable
+    
     //render the variable in a correct span
     counter=counter+1;
     var span=document.getElementById("count");
