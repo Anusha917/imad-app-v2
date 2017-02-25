430de67cbe50e8b3f1fc333d24ab07579912b182
Anusha917 <anusha39393@gmail.com>
2017-02-25 21:33:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,6 +16,9 @@ img.onclick=function(){
     var interval=setInterval(moveRight,50);
     //img.style.marginLeft='100px';//to move image once
 };*///module p5 code
+
+//counter code
+
 var button=document.getElementById("counter");
 var counter=0;
 button.onclick =function(){
@@ -27,5 +30,5 @@ button.onclick =function(){
     counter=counter+1;
     var span=document.getElementById("count");
     span.innerHTML=counter.toString();
-}
+};
 
