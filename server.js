9d565815f224d6671d7aac5e533e945eab949c51
Anusha917 <anusha39393@gmail.com>
2017-02-26 10:52:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,8 +105,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name',function(req,res){
-    var name=req.params.name;
+app.get('/submit-name',function(req,res){//URL:/submit-name?name=XXXXX
+    var name=req.query.name;
     
     names.push(name);
     //JSON_javascript Object Notation
